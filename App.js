90804dd8926fec34897dac7914adfc8e440a5a19
FernandoIvan <fascenciocortes@gmail.com>
2024-11-24 22:19:49 -0600

    Estilo básico

--- App.js
+++ App.js
@@ -80,20 +80,24 @@ export default function App() {
     return(
       // View que contiene todo el contenido
       <View style={styles.tarjeta}>
+        <View style={styles.infoTarjeta}>
           {/* Muestra el nombre del contacto */}
-          <Text>{contacto.nombre}</Text>
+          <Text style={styles.textoTarjeta}>{contacto.nombre}</Text>
           {/* Muestra el nombre del contacto */}
-          <Text>{contacto.telefono}</Text>
+          <Text style={styles.textoTarjeta}>{contacto.telefono}</Text>
           {/* Muestra la descripción del contacto */}
-          <Text>{contacto.descripcion}</Text>
+          <Text style={styles.textoTarjeta}>{contacto.descripcion}</Text>
+        </View>
+        <View style={styles.iconsContainer}>
           {/* Ícono para eliminar la tarea */}
           <Pressable onPress={()=>eliminarContacto(index)}>
-              <FontAwesome name="trash-o" size={32} color="blue"></FontAwesome>
+              <FontAwesome name="trash-o" size={32} color="#000"></FontAwesome>
           </Pressable>
           {/* Ícono para editar la tarea */}
           <Pressable onPress={()=>manejarEdicion(contacto, index)}>
-              <FontAwesome name="pencil" size={32} color="blue"></FontAwesome>
+              <FontAwesome name="pencil" size={32} color="#000"></FontAwesome>
           </Pressable>
+        </View>
       </View>
     )
   }
@@ -118,18 +122,19 @@ export default function App() {
 
   // Renderizado del componente
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={styles.safeAreaProvider}>
       {/* View que contiene la screen principal */}
-      <SafeAreaView style={styles.contenedor}>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.contenedorTituloBoton}>
         {/* Título */}
         <Text style={styles.titulo}>Agenda</Text>
-
         {/* Botón para agregar un nuevo contacto */}
         <Button
           onPress={abrirModal}
           title='Nuevo contacto'
           color='#000000'
         />
+        </View>
         {/* Modal que contiene el formulario para agregar un contacto */}
         <Modal
           animationType="fade"
@@ -137,13 +142,13 @@ export default function App() {
           visible={modalVisible}
           onRequestClose={cerrarModal}
         >
-          <SafeAreaView>
+          <SafeAreaView style={styles.modal}>
             {/*Toast que muestra mensajes al usuario */}
             <Toast />
             {/* Título */}
             <Text style={styles.titulo}>{indiceEdicion !== null ? 'Editar contacto' : 'Nuevo contacto'}</Text>
             {/* Contenedor para los campos */}
-            <View>
+            <View style={styles.campos}>
               {/* Campo para agregar el nombre */}
               <TextInput
                 value={contacto.nombre}
@@ -163,7 +168,7 @@ export default function App() {
                 placeholder='Descripción'
               />
             </View>
-            <View>
+            <View style={styles.botones}>
               {/* Botón para cancelar */}
               <Button
                 onPress={cerrarModal}
@@ -179,36 +184,130 @@ export default function App() {
             </View>
           </SafeAreaView>
         </Modal>
+        <View style={styles.listaContactos}>
         {/* Lista de contactos */}
         <FlatList
           data={contactos}
           renderItem={({item, index})=><Tarjeta contacto={item} index={index} />}
           keyExtractor={(item, index)=>index.toString()}
         />
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  // Estilos del View
-  contenedor: {
+  // Estilos del SafeAreaProvider
+  safeAreaProvider: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  // Estilos del SafeAreaView
+  safeAreaView: {
+    flex: 1,
     justifyContent: 'center',
-  },
-  // Estilos del titulo
-  titulo: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  // Estilos de la tarjeta de contacto
-  tarjeta:{
-    backgroundColor: 'lightgray',
-    marginVertical: 8,
-    marginHorizontal: 16,
+    alignItems: 'center',
     padding: 10,
-    borderRadius: 10
+  },
+
+  // Estilos del contenedor del modal
+  modal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 20,
+  },
+
+  // Estilos para los campos del formulario
+  campos: {
+    width: '100%',
+    marginVertical: 10,
+  },
+
+  // Estilos para los botones dentro del modal
+  botones: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    width: '100%',
+  },
+
+  // Estilos de la tarjeta de contacto
+  tarjeta: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 2,
+  },
+  infoTarjeta: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  textoTarjeta: {
+    marginBottom: 5,
+    fontSize: 14,
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: 80,
+  },
+  // Estilos del contenedor del título y botón de la pantalla principal
+  contenedorTituloBoton: {
+    marginTop: 40,
+    width: '100%',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  // Estilos de la lista de contactos
+  listaContactos: {
+    width: '100%',
+    flex: 1,
+    marginTop: 20,
+  },
+
+  // Estilos de los textos
+  text: {
+    fontSize: 16,
+    marginVertical: 5,
+  },
+
+  // Estilos para los campos de texto de entrada
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+
+  // Estilos del botón
+  button: {
+    width: '45%',
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+  },
+
+  // Estilos de los iconos
+  icon: {
+    marginLeft: 10,
+    marginTop: 5,
   },
 });
